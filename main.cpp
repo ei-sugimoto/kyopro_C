91ceac6b7e3213b73cce3b56a60b132023dbad37
ei-sugimoto <eikun1014@gmail.com>
2024-05-11 11:36:16 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 1; (i) <= (n); ++(i))
+#define ll long long 
+
+int main() {
+    ll int n ,k;
+    cin >> n >> k;
+    ll int p[n];
+    rep(i,n){
+        cin >> p[i];
+    }
+    for(int i = 1; i <= n; i++){
+        
+    }
+}
+
